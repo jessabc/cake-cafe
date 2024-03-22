857004e444ff7f48f89e5efcd9daafff165a7343
jess <120062852+jessabc@users.noreply.github.com>
2024-03-22 11:54:38 +1100

    gallery to add later

--- src/app/(pages)/gallery/page.tsx
+++ src/app/(pages)/gallery/page.tsx
@@ -1,10 +1,10 @@
-// import PageLayout from "@/components/shared/pageLayout/PageLayout";
-// import React from "react";
+import PageLayout from "@/components/shared/pageLayout/PageLayout";
+import React from "react";
 
-// export default function galleryPage() {
-//   return (
-//     <PageLayout header="GALLERY">
-
-//     </PageLayout>
-//   );
-// }
+export default function galleryPage() {
+  return (
+    <PageLayout header="GALLERY">
+      <div>to add later</div>
+    </PageLayout>
+  );
+}

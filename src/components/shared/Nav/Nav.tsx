49f55b2a7995271aae587b771487900609ef0435
jess <120062852+jessabc@users.noreply.github.com>
2024-03-27 14:46:41 +1100

    add mobnav

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import NavLink from "./NavLink";
 import "./nav.css";
+import MobNav from "./MobNav";
 
 // https://alvarotrigo.com/blog/hamburger-menu-css/
 // 10. Full Screen Morphing Hamburger Menu
@@ -18,22 +19,29 @@ export default function Nav() {
     setIsOpen((prev) => !prev);
   };
   return (
-    <div className="">
-      <div
-        onClick={handleClick}
-        className="hamburger-container h-7 w-8 fixed top-6 right-6 z-[300] opacity-40 cursor-pointer"
-      ></div>
-      <span className={`hamburger ${isOpen ? "hamburger-toggle" : ""} `}></span>
+    <>
+      <div className="hidden md:block">
+        <div
+          onClick={handleClick}
+          className="hamburger-container h-7 w-8 fixed top-6 right-6 z-[300] opacity-40 cursor-pointer  "
+        ></div>
+        <span
+          className={`hamburger ${isOpen ? "hamburger-toggle" : ""}  `}
+        ></span>
 
-      <div className={`menu ${isOpen ? "menu-toggle " : " "}`}></div>
+        <div className={`menu ${isOpen ? "menu-toggle " : " "}  `}></div>
 
-      <div
-        className={`flex flex-col gap-8 links ${
-          isOpen ? " opacity-100" : "opacity-0 pointer-events-none"
-        }`}
-      >
-        {navLinkEl}
+        <div
+          className={`flex flex-col gap-8 links ${
+            isOpen ? "md:opacity-100" : "opacity-0 pointer-events-none"
+          } `}
+        >
+          {navLinkEl}
+        </div>
       </div>
-    </div>
+      <div className="md:hidden">
+        <MobNav />
+      </div>
+    </>
   );
 }

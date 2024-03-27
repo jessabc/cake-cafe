971dbfd747e0c9c43407109a2093536f427f5dee
jess <120062852+jessabc@users.noreply.github.com>
2024-03-27 14:42:39 +1100

    remove carousel on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import MobHero from "@/components/landing/MobHero";
 import Carousel from "@/components/landing/carousel/Carousel";
 import PostsSection from "@/components/landing/postSection/PostsSection";
 import TopSection from "@/components/landing/topSection/TopSection";
@@ -5,7 +6,13 @@ import TopSection from "@/components/landing/topSection/TopSection";
 export default function Home() {
   return (
     <main className="">
-      <Carousel />
+      <div className="hidden md:block">
+        <Carousel />
+      </div>
+      <div className="md:hidden">
+        <MobHero />
+      </div>
+
       <div className=" mx-5 md:mx-10">
         <TopSection />
         <PostsSection />

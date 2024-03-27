756e8ad75e3c30585e07b3728091ea9136220528
jess <120062852+jessabc@users.noreply.github.com>
2024-03-27 14:42:51 +1100

    add mob hero

---
+++ src/components/landing/MobHero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function MobHero() {
+  return (
+    <div className="h-1/2 bg-violet-500 ">
+      <img src="image12.jpg" alt="dessert image" className="opacity-80 " />
+      <span className="self-center text-4xl transition duration-700 absolute ml-auto mr-auto left-0 right-0 top-56 text-center text-violet-100 tracking-widest font-bold ">
+        citrus loves dreaming
+      </span>
+    </div>
+  );
+}
